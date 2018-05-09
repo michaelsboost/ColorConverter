8d7f26b48bb65023cf4f17671d6734b343442292
Michael <mikethedj4@yahoo.com>
2018-05-09 13:04:36 -0500

    Remember color values

--- js/app.js
+++ js/app.js
@@ -1,21 +1,39 @@
 var input  = document.querySelector('[data-color=input]'),
     output = document.querySelector('[data-color=output]'),
-    btn = document.querySelector('[data-call=colorstring]'),
+    btn    = document.querySelector('[data-call=colorstring]'),
     lightHeading = "hsl(220, 4%, 85%)",
-    darkHeading  = "hsl(214, 4%, 34%)";
+    darkHeading  = "hsl(214, 4%, 34%)",
+    initializeLocalStorage = function() {
+      // remember button text
+      if ( localStorage.getItem("convertTo")) {
+        btn.textContent = localStorage.getItem("convertTo");
+      }
+      // remember input value
+      if ( localStorage.getItem("inputVal")) {
+        input.value = localStorage.getItem("inputVal");
+      }
+      // remember output value
+      if ( localStorage.getItem("outputVal")) {
+        output.value = localStorage.getItem("outputVal");
+      }
+    };
 
 var colorConverstion = function() {
   var color  = tinycolor(input.value);
-  
+  localStorage.setItem("inputVal", input.value);
+
   if (input.value) {
     if (color.isValid()) {
       // place output color
       if (btn.textContent === "HEX") {
         output.value = color.toHexString();
+        localStorage.setItem("outputVal", output.value);
       } else if (btn.textContent === "RGB") {
         output.value = color.toRgbString();
+        localStorage.setItem("outputVal", output.value);
       } else if (btn.textContent === "HSL") {
         output.value = color.toHslString();
+        localStorage.setItem("outputVal", output.value);
       }
       
       // output color as body background
@@ -29,22 +47,27 @@ var colorConverstion = function() {
       }
     } else {
       output.value = "No valid color detected!";
+      localStorage.setItem("outputVal", output.value);
     }
     return false;
   } else {
     return false;
   }
 }
+initializeLocalStorage();
 colorConverstion();
 
 // changes color output
 btn.onclick = function() {
   if (this.textContent === "RGB") {
     this.textContent = "HSL";
+    localStorage.setItem("convertTo", this.textContent);
   } else if (this.textContent === "HSL") {
     this.textContent = "HEX";
+    localStorage.setItem("convertTo", this.textContent);
   } else if (this.textContent === "HEX") {
     this.textContent = "RGB";
+    localStorage.setItem("convertTo", this.textContent);
   }
   colorConverstion();
 };
